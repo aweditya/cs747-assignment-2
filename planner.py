8e51292b08296e85bfb0035b5b23cb1e1f6f8138
Aditya Sriram <aweditya@gmail.com>
2022-09-29 22:50:25 +0530

    Done with value iteration and policy evaluation

--- planner.py
+++ planner.py
@@ -7,8 +7,37 @@ class Planner():
     def __init__(self,mdp,algorithm,policy):
         S, A, T, R, gamma = read_mdp(mdp)
 
+        if algorithm == "vi":
+            if policy == None:
+                V_star, pi_star = vi(S, A, T, R, gamma)
+                print_result(V_star, pi_star)
+            else:
+                policy = read_policy(policy)
+                V_pi = policy_eval(S, A, T, R, gamma, policy)
+                print_result(V_pi, policy)
+        elif algorithm == "hpi":
+            if policy == None:
+                hpi(S, A, T, R, gamma)
+            else:
+                policy = read_policy(policy)
+                V_pi = policy_eval(S, A, T, R, gamma, policy)
+                print_result(V_pi, policy)
+        elif algorithm == "lp":
+            if policy == None:
+                lp(S, A, T, R, gamma)
+            else:
+                policy = read_policy(policy)
+                V_pi = policy_eval(S, A, T, R, gamma, policy)
+                print_result(V_pi, policy)
+        elif algorithm == "dual_lp":
+            if policy == None:
+                dual_lp(S, A, T, R, gamma)
+            else:
+                policy = read_policy(policy)
+                V_pi = policy_eval(S, A, T, R, gamma, policy)
+                print_result(V_pi, policy)
+
 def read_mdp(path):
-    T = R = dict()
     with open(path, 'r') as f:
         lines = f.readlines()
         for line in lines:
@@ -17,7 +46,8 @@ def read_mdp(path):
                 S = int(split_line[1])
             elif split_line[0] == "numActions":
                 A = int(split_line[1])
-                T = R = np.zeros((S, A, S))
+                R = np.zeros((S, A))
+                T = np.zeros((S, A, S))
             elif split_line[0] == "end":
                 end_states = map(int, split_line[1])
             elif split_line[0] == "transition":
@@ -26,8 +56,8 @@ def read_mdp(path):
                 s2 = int(split_line[3])
                 r = float(split_line[4])
                 p = float(split_line[5])
-                R[s1, a, s2] = r
-                T[s1, a, s2] = p
+                R[s1, a] += p*r
+                T[s1, a, s2] += p
             elif split_line[0] == "mdptype":
                 mdptype = split_line[1]
             elif split_line[0] == "discount":
@@ -35,10 +65,46 @@ def read_mdp(path):
 
     if mdptype == "episodic":
         for ed in end_states:
-            R[ed, :, ed] = 0
+            R[ed, :] = 0
             T[ed, :, ed] = 1
-
+            
     return S, A, T, R, gamma
+
+def read_policy(path):
+    policy = []
+    with open(path, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            policy.append(int(line.strip()))
+
+    return policy
+
+def vi(S, A, T, R, gamma):
+    V_star = np.zeros(S)
+    while(True):
+        V_star_next = np.max(R + np.sum(gamma * T * V_star.reshape(1, 1, S), axis=2), axis=1)
+        if np.allclose(V_star, V_star_next, rtol=1e-11, atol=1e-8):
+            pi_star = np.argmax(R + np.sum(gamma * T * V_star.reshape(1, 1, S), axis=2), axis=1)
+            return V_star_next, pi_star
+        else:
+            V_star = V_star_next
+
+def hpi(S, A, T, R, gamma):
+    pass
+
+def lp(S, A, T, R, gamma):
+    pass
+
+def dual_lp(S, A, T, R, gamma):
+    pass
+
+def policy_eval(S, A, T, R, gamma, policy):
+    V_pi = np.linalg.pinv(np.eye(S) - gamma * T[np.arange(S), policy, :]) @ R[np.arange(S), policy]
+    return V_pi
+
+def print_result(V, pi):
+    for value, action in zip(V, pi):
+        print(value, action)
 
 if __name__ == "__main__":
     parser.add_argument("--mdp",type=str,required=True,help="Path to input MDP file")
@@ -46,6 +112,5 @@ if __name__ == "__main__":
     parser.add_argument("--policy",type=str,help="Policy file for which V^pi is to be evaluated (optional)")
     
     args = parser.parse_args()
-    # print(args)
     
     planner = Planner(args.mdp,args.algorithm,args.policy)
