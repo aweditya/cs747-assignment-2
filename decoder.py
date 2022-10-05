56af2e13638ecbe2f7c7e59160d2ba9dd044cc95
Aditya Sriram <aweditya@gmail.com>
2022-10-05 13:54:05 +0530

    Decoder will only the print the optimal policy, value function for the states of interest

--- decoder.py
+++ decoder.py
@@ -1,35 +1,46 @@
 #! /usr/bin/python
 import argparse
-from audioop import reverse
-from email import policy
 parser = argparse.ArgumentParser()
 
 class Decoder():
     def __init__(self,value_policy,states):
-        O, T = read_states(states)
-        decode(value_policy, O, T)
+        states, O, T = read_states(states)
+        decode(states, value_policy, O, T)
 
 def read_states(path):
+    states = []
+    O = 0
+    T = 0
     with open(path, 'r') as f:
         lines = f.readlines()
+        for line in lines:
+            line = line.strip()
 
-    O = 15
-    T = 10
-    return O, T
+            o = int(line[0:2])
+            t = int(line[2:4])
+            states.append([o, t])
+
+            O = max(o, O)
+            T = max(t, T)
+
+    return states, O, T
+
+def state_map(o, t, O, T):
+    return o * (T + 1) + t
 
 def action_map(ac):
     action = {0: 0, 1: 1, 2: 2, 3: 4, 4: 6}
     return action[ac]
 
-def decode(value_policy, O, T):
+def decode(states, value_policy, O, T):
     with open(value_policy, 'r') as f:
         lines = f.readlines()
         split_lines = [line.strip().split() for line in lines]
-        for o in reversed(range(O+1)):
-            for t in reversed(range(T+1)):
-                if not (o == 0 or t == 0):
-                    print(str(o).zfill(2) + str(t).zfill(2) + " " + str(action_map(int(split_lines[o*(T+1) + t][1]))) + " " + split_lines[o*(T+1) + t][0])
-
+        for state in states:
+            o = state[0]
+            t = state[1]
+            id = state_map(o, t, O, T)
+            print(str(o).zfill(2) + str(t).zfill(2) + " " + str(action_map(int(split_lines[id][1]))) + " " + split_lines[id][0])
 
 if __name__ == "__main__":
     parser.add_argument("--value-policy",type=str,required=True,help="Path to file containing optimal value function and policy")
